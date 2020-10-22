6cd18f7d26e5584d5b737e8d86dd08c2c355d0df
ZimGil <gil.tichon@gmail.com>
2020-10-22 14:52:18 +0300

    add tests

---
+++ test/toMilliseconds.specs.js
@@ -0,0 +1,86 @@
+const expect = require('chai').expect;
+const toMilliseconds = require('../toMilliseconds');
+
+describe('toMilliseconds()', () => {
+  it('Should return correct number for single value', () => {
+    const input = '1w';
+    const expected = 1000 * 60 * 60 * 24 * 7;
+    expect(() => toMilliseconds(input)).to.not.throw();
+    const actual = toMilliseconds(input);
+    expect(actual).to.be.a('number');
+    expect(actual).to.be.equal(expected);
+  });
+
+
+  it('Should return correct number for multiple values', () => {
+    const input = '1y 1w';
+    const expected = (1000 * 60 * 60 * 24 * 365) + (1000 * 60 * 60 * 24 * 7);
+    expect(() => toMilliseconds(input)).to.not.throw();
+    const actual = toMilliseconds(input);
+    expect(actual).to.be.a('number');
+    expect(actual).to.be.equal(expected);
+  });
+
+
+  it('Should return correct number for repeadted values', () => {
+    const input = '1y 1y 1y 2w 2w 2w';
+    const expected = 3 * (1000 * 60 * 60 * 24 * 365) + 6 * (1000 * 60 * 60 * 24 * 7);
+    expect(() => toMilliseconds(input)).to.not.throw();
+    const actual = toMilliseconds(input);
+    expect(actual).to.be.a('number');
+    expect(actual).to.be.equal(expected);
+  });
+
+  it('Should return correct number for empty string', () => {
+    const input = '';
+    const expected = 0;
+    expect(() => toMilliseconds(input)).to.not.throw();
+    const actual = toMilliseconds(input);
+    expect(actual).to.be.a('number');
+    expect(actual).to.be.equal(expected);
+  });
+
+
+  it('Should return correct number for number string', () => {
+    const input = '100';
+    const expected = 100;
+    expect(() => toMilliseconds(input)).to.not.throw();
+    const actual = toMilliseconds(input);
+    expect(actual).to.be.a('number');
+    expect(actual).to.be.equal(expected);
+  });
+
+
+  it('Should return correct number for number value', () => {
+    const input = 100;
+    const expected = 100;
+    expect(toMilliseconds.bind(null, input)).to.not.throw();
+    const actual = toMilliseconds(input);
+    expect(actual).to.be.a('number');
+    expect(actual).to.be.equal(expected);
+  });
+
+  it('Should throw an error if no input', () => {
+    const expected = 'Unexpected value type: [undefined] is not supported, expected [string]';
+    expect(() => toMilliseconds()).to.throw(expected);
+  });
+
+  it('Should throw an error if unsupported input type', () => {
+    const input = { foo: 'bar' };
+    const expected = 'Unexpected value type: [object] is not supported, expected [string]';
+    expect(() => toMilliseconds(input)).to.throw(expected);
+  });
+
+  it('Should throw an error if unsupported value in input', () => {
+    const input = '1y foobar 2h';
+    const expected = 'Unexpected value: foobar is not a supported value';
+    expect(() => toMilliseconds(input)).to.throw(expected);
+  });
+
+  it('Should throw an error if unsupported time identifier', () => {
+    const input = '1y 1f 2h';
+    const expected = 'Unexpected value: f is not a supported time identifier';
+    expect(() => toMilliseconds(input)).to.throw(expected);
+  });
+});
+
---
+++ test/toReadable.specs.js
@@ -0,0 +1,60 @@
+const expect = require('chai').expect;
+const toReadable = require('../toReadable');
+
+describe('toReadable()', () => {
+  it('Should return empty string for input (0)', () => {
+    const input = 0;
+    const expected = '';
+    expect(() => toReadable(input)).to.not.throw();
+    const actual = toReadable(input);
+    expect(actual).to.be.a('string');
+    expect(actual).to.be.equal(expected);
+  });
+
+  it('Should return highest readable time as possible', () => {
+    const input = 1000 * 60;
+    const expected = '1m';
+    expect(() => toReadable(input)).to.not.throw();
+    const actual = toReadable(input);
+    expect(actual).to.be.a('string');
+    expect(actual).to.be.equal(expected);
+  });
+
+  it('Should return single readable time for multipications', () => {
+    const input = 1000 * 60 * 60 * 24 * 365 * 2;
+    const expected = '2y';
+    expect(() => toReadable(input)).to.not.throw();
+    const actual = toReadable(input);
+    expect(actual).to.be.a('string');
+    expect(actual).to.be.equal(expected);
+  });
+
+  it('Should be able to use all identifiers correctly', () => {
+    const input = 38898367008;
+    const expected = '1y 2mo 3w 4d 5h 6m 7s 8ms';
+    expect(() => toReadable(input)).to.not.throw();
+    const actual = toReadable(input);
+    expect(actual).to.be.a('string');
+    expect(actual).to.be.equal(expected);
+  });
+
+  it('Should return correctly when input is string number', () => {
+    const input = '38898367008';
+    const expected = '1y 2mo 3w 4d 5h 6m 7s 8ms';
+    expect(() => toReadable(input)).to.not.throw();
+    const actual = toReadable(input);
+    expect(actual).to.be.a('string');
+    expect(actual).to.be.equal(expected);
+  });
+
+  it('Should throw an error when input is NaN', () => {
+    const input = { foo: 'bar' };
+    const expected = 'Unexpected value: [object Object] is not convertable to number';
+    expect(() => toReadable(input)).to.throw(expected);
+  });
+
+  it('Should throw an error when no input', () => {
+    const expected = 'Unexpected value: undefined is not convertable to number';
+    expect(() => toReadable()).to.throw(expected);
+  });
+});
