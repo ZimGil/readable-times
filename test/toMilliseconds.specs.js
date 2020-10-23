6b1e3b1cb3317c43c8d51fe597aa8cf30ebc10f5
ZimGil <gil.tichon@gmail.com>
2020-10-23 19:05:01 +0300

    Make toMilliseconds regexp case insensitive

--- test/toMilliseconds.specs.js
+++ test/toMilliseconds.specs.js
@@ -60,6 +60,76 @@ describe('toMilliseconds()', () => {
     expect(actual).to.be.equal(expected);
   });
 
+  it('Should return correct number when input use any of possible identifiers', () => {
+    const input = [
+      "1milliseconds",
+      "1millisec",
+      "1millisec",
+      "1ms",
+      "1seconds",
+      "1second",
+      "1secs",
+      "1sec",
+      "1s",
+      "1minutes",
+      "1minute",
+      "1mins",
+      "1min",
+      "1m",
+      "1hours",
+      "1hour",
+      "1h",
+      "1days",
+      "1day",
+      "1d",
+      "1weeks",
+      "1week",
+      "1w",
+      "1months",
+      "1month",
+      "1mo",
+      "1years",
+      "1year",
+      "1y"
+    ].join(' ')
+
+    const expected =
+      1 + // "1milliseconds",
+      1 + // "1millisec",
+      1 + // "1millisec",
+      1 + // "1ms",
+      1 * 1000 + // "1seconds",
+      1 * 1000 + // "1second",
+      1 * 1000 + // "1secs",
+      1 * 1000 + // "1sec",
+      1 * 1000 + // "1s",
+      1 * 1000 * 60 + // "1minutes",
+      1 * 1000 * 60 + // "1minute",
+      1 * 1000 * 60 + // "1mins",
+      1 * 1000 * 60 + // "1min",
+      1 * 1000 * 60 + // "1m",
+      1 * 1000 * 60 * 60 + // "1hours",
+      1 * 1000 * 60 * 60 + // "1hour",
+      1 * 1000 * 60 * 60 + // "1h",
+      1 * 1000 * 60 * 60 * 24 + // "1days",
+      1 * 1000 * 60 * 60 * 24 + // "1day",
+      1 * 1000 * 60 * 60 * 24 + // "1d",
+      1 * 1000 * 60 * 60 * 24 * 7 + // "1weeks",
+      1 * 1000 * 60 * 60 * 24 * 7 + // "1week",
+      1 * 1000 * 60 * 60 * 24 * 7 + // "1w",
+      1 * 1000 * 60 * 60 * 24 * 30 + // "1months",
+      1 * 1000 * 60 * 60 * 24 * 30 + // "1month",
+      1 * 1000 * 60 * 60 * 24 * 30 + // "1mo",
+      1 * 1000 * 60 * 60 * 24 * 365 + // "1years",
+      1 * 1000 * 60 * 60 * 24 * 365 + // "1year",
+      1 * 1000 * 60 * 60 * 24 * 365;   // "1y"
+
+      expect(() => toMilliseconds(input)).to.not.throw();
+      const actual = toMilliseconds(input);
+      expect(actual).to.be.a('number');
+      expect(actual).to.be.equal(expected);
+  });
+
   it('Should return correct number for empty string', () => {
     const input = '';
     const expected = 0;
@@ -93,6 +163,16 @@ describe('toMilliseconds()', () => {
     const input = 100;
     const expected = 100;
     expect(toMilliseconds.bind(null, input)).to.not.throw();
+    const actual = toMilliseconds(input);
+    expect(actual).to.be.a('number');
+    expect(actual).to.be.equal(expected);
+  });
+
+  it('Should be case insensitive', () => {
+    const input = '1yEaR'
+    expected = 1000 * 60 * 60 * 24 * 365;
+
+    expect(() => toMilliseconds(input)).to.not.throw();
     const actual = toMilliseconds(input);
     expect(actual).to.be.a('number');
     expect(actual).to.be.equal(expected);
